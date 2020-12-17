e9dfcffaacdfa6fe9238a7d69c48ff5ab73ff6cf
meghanaraobn2020 <76180138+meghanaraobn2020@users.noreply.github.com>
2020-12-18 00:31:37 +0530

    Update SearchFiles.java

--- SearchFiles.java
+++ SearchFiles.java
@@ -1,1 +1,38 @@
+	  
+	  public static void searchIndex(String indexPath, String queryTerms, int hits ) throws IOException, ParseException {
 
+		  Directory index = FSDirectory.open(Paths.get(indexPath));
+		  
+		  IndexReader indexReader = DirectoryReader.open(index);
+		  IndexSearcher indexSearcher = new IndexSearcher(indexReader);
+		  
+		  TopScoreDocCollector collector = TopScoreDocCollector.create(hits);
+		  
+
+		  Analyzer analyzer = new EnglishAnalyzer();
+	  
+	    QueryParser queryParser = new QueryParser("DocContents",analyzer); 
+	    Query query = queryParser.parse(queryTerms);
+
+	    indexSearcher.search(query, collector);
+			  
+		  ScoreDoc[] scoreHits =collector.topDocs().scoreDocs;
+			  
+		  for(int j=0; j<scoreHits.length; j++) {
+        
+			  int docID = scoreHits[j].doc;
+			  Document d = indexSearcher.doc(docID);
+			  System.out.println(d.get("DocName"));
+		  
+		  }
+		  
+		  System.out.println("Found:" + scoreHits.length);
+
+		  
+		  analyzer.close();
+
+		  indexReader.close();
+
+		  
+	  }
+	  
