cc529324df5de4d192c1b7d4ab5880db1894e77f
meghanaraobn2020 <76180138+meghanaraobn2020@users.noreply.github.com>
2020-12-18 00:28:31 +0530

    Update Index.java

--- Index.java
+++ Index.java
@@ -11,15 +11,85 @@ import org.apache.lucene.store.FSDirectory;
 public class Index {
 
 	 public static int indexFiles(String indexPath, File dataDir) throws IOException {
-		  return 1;
+		 
+		  Directory index = FSDirectory.open(Paths.get(indexPath));
+		  
+		  Analyzer analyzer = new EnglishAnalyzer();
+	  
+		  IndexWriterConfig indexWriterConfig = new IndexWriterConfig(analyzer);
+		  indexWriterConfig.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
+		  
+		  IndexWriter indexWriter = new IndexWriter(index, indexWriterConfig);
+		  
+		  indexDirectory(indexWriter, dataDir,analyzer);
+		  
+		  int numIndexed = indexWriter.maxDoc();
+		  indexWriter.close();
+
+	      return numIndexed;
 
 	  }
 	  
 	  public static void indexDirectory(IndexWriter indexWriter, File dataDir) {
 		  
+		  File[] files = dataDir.listFiles();
+		  
+		  for(int i =0; i<files.length; i++) {
+			  File file = files[i];
+			  
+			  if(file.isDirectory()) {
+				  indexDirectory(indexWriter, file, analyzer);
+			  }
+			  
+			  else {
+				  try {
+					indexFileWithIndexWriter(indexWriter, file);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			  }
+		  }
+  
+		  
 	  }
 	  
 	  public static void indexFileWithIndexWriter(IndexWriter indexWriter, File file) throws IOException {
+		  
+		  if(file.isHidden() || file.isDirectory() || !file.canRead() || !file.exists()) {
+			  return;
+		  }
+		  
+		  if(file.getName().endsWith(".txt") || file.getName().endsWith(".html") || file.getName().endsWith(".htm")) {
+			  
+			  
+			  BufferedReader  reader = new BufferedReader (new FileReader(file));
+			  StringBuilder stringBuilder = new StringBuilder();
+			  
+			  String line = null;
+			  String ls = System.getProperty("line.separator");
+			  while ((line = reader.readLine()) != null) {
+			  	stringBuilder.append(line);
+			  	stringBuilder.append(ls);
+			  }
+
+			  stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+			  reader.close();
+			  
+			  String docContents = stringBuilder.toString();
+			  
+			  System.out.println("Indexing File:...." + file.getCanonicalPath());
+			  Document doc = new Document();
+			  doc.add(new TextField("DocName", file.getName(), Field.Store.YES));
+			  doc.add(new TextField("DocContents",docContents, Field.Store.YES ));
+			  doc.add(new TextField("DocPath", file.getCanonicalPath(), Field.Store.YES));
+			 
+			  indexWriter.addDocument(doc);
+		  }
+		  
+		  else {
+			  return;
+		  }
+	  
 		  
 		    
 		  
