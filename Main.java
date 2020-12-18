e0a94cd268ce8e008f61f3fec07ccefafa652af7
dianacgr <dcguzman.20@gmail.com>
2020-12-18 17:40:09 +0100

    Include documentation Main

--- Main.java
+++ Main.java
@@ -8,10 +8,14 @@ public class Main {
 		String inputPath = null;
 		String indexPath = "index";
 		boolean performIndexing = true;
+		//Check the amount of arguments passed by the user in the console
 		if (args.length != 1) {
             System.out.println("Invalid amount of arguments. To execute the file use java -jar IR_P01.jar [path to document folder]");
         } else {
+        	//First argument is the path to the document folder
             inputPath = args[0];
+            //In case the empty string is included it checks whether the user wants to use the current folder as 
+            //the document folder. In case it is not desired finish the execution 
             if (inputPath.equals("")) {
                 System.out.println("Do you want to use the current folder as the document folder? [Y/N]");
                 String UserInput;
