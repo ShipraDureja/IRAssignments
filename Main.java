1f33e518a80e74d545fc099a7deaa980b30f4572
dianacgr <dcguzman.20@gmail.com>
2020-12-18 17:11:15 +0100

    Change in Main file

--- Main.java
+++ Main.java
@@ -1,24 +1,43 @@
-public class Main {
-    public static void main(String[] args) throws Exception {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
-        String inputPath = "C:\\Users\\Shipra Dureja\\Downloads\\documents";
-        String indexPath = "index";
-        Indexing.indexing(inputPath, indexPath);
-        SearchIndex.searching(indexPath);
-        /*String inputPath = null;
-        String indexPath = "index";
-        if (args.length != 1) {
-            System.out.println("Invalid arguments");
+public class Main {
+	public static void main(String args[]){
+		String inputPath = null;
+		String indexPath = "index";
+		boolean performIndexing = true;
+		if (args.length != 1) {
+            System.out.println("Invalid amount of arguments. To execute the file use java -jar IR_P01.jar [path to document folder]");
         } else {
             inputPath = args[0];
-            if (inputPath != null) {
-                Indexing.indexing(inputPath, indexPath);
-            }try {
-                SearchIndex.searching(indexPath);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }*/
-    }
+            if (inputPath.equals("")) {
+                System.out.println("Do you want to use the current folder as the document folder? [Y/N]");
+                String UserInput;
+                BufferedReader bReader = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
+                try {
+                	UserInput = bReader.readLine();
+					if(!UserInput.equalsIgnoreCase("y")) {
+						performIndexing = false;
+						System.out.println("To run the project with a different path please use java -jar IR_P01.jar [path to document folder]");
+					}
+				} catch (IOException e) {
+					System.out.println(e.getMessage());
+				}
+            } 
+            try {
+            	if (performIndexing) {
+                	//Index input folder
+                	Indexing.indexing(inputPath, indexPath);
+                	//Query over indexed files
+                	SearchIndex.searching(indexPath);
+            	}
+    		} catch (Exception e) {
+    			System.out.println("An error was encounter during indexing process. Error:" + e.getMessage());
+    		}
+        }
+		
+		
+	}
 }
-
